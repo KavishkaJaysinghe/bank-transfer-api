da4db0d58ae0c6b5be2805239ea6bd98a41ae546
Kavishka Jayasinghe <kavishkajaye@gmail.com>
2025-03-10 10:23:02 +0530

    Test Warning fix

--- src/main/java/com/bankapi/bank_transfer_api/model/Transaction.java
+++ src/main/java/com/bankapi/bank_transfer_api/model/Transaction.java
@@ -2,10 +2,8 @@ package com.bankapi.bank_transfer_api.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.Setter;
 
 @Getter
-@Setter
 @AllArgsConstructor
 public class Transaction {
     private String sourceAccountNumber;
